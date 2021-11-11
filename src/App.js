850263524fb8938a67a3172ade4e18db5b49c54e
scarycool11 <79058996+scarycool11@users.noreply.github.com>
2021-11-11 20:16:59 +0900

    NavBar complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-
-import { Button, Alert, Breadcrumb } from 'react-bootstrap'
+import NavBar from './Components/NavBar';
+import { Button, Alert, Breadcrumb, Container, Row, Col, Navbar } from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <Breadcrumb>
-         <Breadcrumb.Item>Test</Breadcrumb.Item>
-         <Breadcrumb.Item>Test</Breadcrumb.Item>
-         <Breadcrumb.Item active>Test</Breadcrumb.Item>
-       </Breadcrumb>
-        <Alert variant="primary">This is a Button</Alert>
-        <Button>Test Button</Button>
+    
+      <NavBar />
+      
+      
         
     
-      </header>
-    </div>
+      
+    
   );
 }
 
